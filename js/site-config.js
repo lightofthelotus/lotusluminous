4758dad390cb97e533f94276f50b32e94fabc83c
lightofthelotus <apadmaprabha@yahoo.com>
2026-07-24 22:38:29 +0530

    Modify basePath assignment in site-config.js
    Change basePath to use projectPath directly without leading slash.

--- js/site-config.js
+++ js/site-config.js
@@ -1,6 +1,6 @@
 window.SiteConfig = (function () {
   const projectPath = '';//'lotusluminous';
-  const basePath = `/${projectPath}`;
+  const basePath = `${projectPath}`;//`/${projectPath}`;
 
   function assetPath(path) {
     return `${basePath}/${String(path).replace(/^\/+/, '')}`;
